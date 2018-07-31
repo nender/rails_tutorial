e70a5c460c88e3632b3f3cce0041bad630295053
nender <nathan.ender@gmail.com>
2018-07-31 20:37:54 +0200

    End of 14.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,15 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -40,17 +49,32 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
-    def activate
-      update_columns(activated: true, activated_at: Time.zone.now)
-    end
-  
-    def send_activation_email
-      UserMailer.account_activation(self).deliver_now
-    end
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
 
-    def feed
-      Micropost.where("user_id = ?", id)
-    end
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following.push(other_user)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
   private
     def downcase_email
